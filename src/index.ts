1e7d6dee1768cbf492af8fd45e3137934ba62c8f
Paul Robello <probello@xojetaviation.com>
2023-01-07 18:18:18 -0800

    disable hmr hack

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import { World } from './World';
 const world: World = new World();
 world.draw();
 
-if (module.hot) {
-  module.hot.accept(() => {
-    // console.log('module.hot index accepted');
-    // window.location.reload();
-  });
-
-  module.hot.dispose(() => {
-    console.log('module.hot index disposed');
-    window.location.reload();
-  });
-}
+// if (module.hot) {
+//   module.hot.accept(() => {
+//     // console.log('module.hot index accepted');
+//     // window.location.reload();
+//   });
+//
+//   module.hot.dispose(() => {
+//     console.log('module.hot index disposed');
+//     window.location.reload();
+//   });
+// }
